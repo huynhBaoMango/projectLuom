b2a8799b5fe83677bd84f84bd911e4393dac2485
duytammm <nguyenduytam20042004@gmail.com>
2023-08-03 11:49:40 +0700

    chognhi

--- Assets/chonghi.cs
+++ Assets/chonghi.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class baongu : MonoBehaviour
+public class chonghi : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
