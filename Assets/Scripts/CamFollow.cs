ca0312fb7f9e5890d51f7fe60d96cb6c391f2573
huynhBaoMango <deathtnt.thebest@gmail.com>
2023-08-23 10:22:31 +0700

    add sprite, them code player, code camera

---
+++ Assets/Scripts/CamFollow.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamFollow : MonoBehaviour
+{
+    public float flspeed = 2f;
+    public Transform target;
+    public float yOffset = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 newPos = new Vector3(target.position.x,target.position.y,-10f);
+        transform.position = Vector3.Slerp(transform.position, newPos, flspeed * Time.deltaTime);
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,33 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    float tocdo;
+    float huongdichuyen;
     // Start is called before the first frame update
     void Start()
     {
-        
+        tocdo = 10;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKey(KeyCode.RightArrow)){
+            transform.Translate(Vector3.right * tocdo * Time.deltaTime);
+        }   
+        if (Input.GetKey(KeyCode.LeftArrow)){
+            transform.Translate(Vector3.left * tocdo * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.UpArrow)){
+            transform.Translate(Vector3.up * tocdo * Time.deltaTime);
+        }
+        if (Input.GetKey(KeyCode.DownArrow)){
+            transform.Translate(Vector3.down * tocdo * Time.deltaTime);
+        }
+    }
+
+    public Vector3 GetPosition()
+    {
+        return transform.position;
     }
 }
