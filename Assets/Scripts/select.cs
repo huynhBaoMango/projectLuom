f42be4535423d975b21c4cf872e0b8eecced5821
hnghii <hnghi201003@gmail.com>
2023-08-24 13:25:13 +0700

    menu paused,over,start

---
+++ Assets/Scripts/select.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+
+public class select : MonoBehaviour
+{
+  [SerializeField] private RectTransform[] options;
+ // [SerializeField] private AudioClip changeSound;//nhac khi chon len xuong
+  //[SerializeField] private AudioClip interactSound;//nhac khi chon
+  private RectTransform rect;
+  private int currentPosition;
+
+  private void Awake()
+  {
+    rect = GetComponent<RectTransform>();
+  }
+
+  private void Update()
+  {
+    if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow)) 
+    changePosition(-1);
+    if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+    changePosition(1);
+  }
+  private void changePosition(int _change)
+  {
+    currentPosition += _change;
+
+    //if(_change != 0)
+    //SoundManager.instance.PlaySound(changeSound);
+  
+    if(currentPosition < 0)
+    currentPosition = options.Length - 1;
+    else if (currentPosition > options.Length - 1)
+    currentPosition = 0;
+
+    //gan mui ten di chuyen len xuong
+    rect.position=new Vector3(rect.position.x,options[currentPosition].position.y, 0);
+  }
+}
