f0fb7450ea61bd3af0979a41f62aced5ec70ab15
huynhBaoMango <deathtnt.thebest@gmail.com>
2023-08-26 20:45:46 +0700

    sua menubatdau, them thoi gian dem nguoc

---
+++ Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class CountdownTimer : MonoBehaviour
+{
+    [SerializeField] TextMeshProUGUI text;
+    float timeHientai;
+    float timetoCount = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeHientai = timetoCount;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeHientai -= 1 * Time.deltaTime;
+        text.text = timeHientai.ToString("0");
+        if(timeHientai <= 0)
+        {
+            timeHientai= 0;
+        }
+    }
+}
