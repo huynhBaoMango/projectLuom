e67eef7d56c78c25d2333fc0152565a5a776d38b
hnghii <hnghi201003@gmail.com>
2023-09-08 16:49:11 +0700

    done key để mở cửa

--- Assets/Scripts/mocua.cs
+++ Assets/Scripts/mocua.cs
@@ -4,25 +4,24 @@ using UnityEngine;
 
 public class mocua : MonoBehaviour
 {
-
-    [SerializeField] private AudioClip openSound;
-
-    void OnTriggerEnter2D(Collider2D other)
+    public bool isLocked = true; // Biến để kiểm tra xem cánh cửa có bị khóa hay không
+    
+    void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.CompareTag("Player"))
+        // Kiểm tra xem đối tượng va chạm có phải là người chơi hay không
+        if (other.gameObject.CompareTag("Player"))
         {
-            if (chiakhoa.itemCount > 0)
+            // Kiểm tra xem người chơi có chìa khóa cần thiết để mở cánh cửa hay không
+            if (chiakhoa.itemCount == 0)
             {
-                AudioSource.PlayClipAtPoint(openSound,transform.position);
-                chiakhoa.itemCount--;
-                Destroy(gameObject);
-
+                // Mở khóa cánh cửa và cho phép người chơi đi qua
+                isLocked = false;
+                gameObject.SetActive(false); // Ẩn cánh cửa
             }
             else
             {
-                // Người chơi chưa nhặt được chiaf khoa
+                // Người chơi chưa nhặt được đủ 1 key
             }
-
         }
     }
 }
--- Assets/Scripts/vatpham/chiakhoa.cs
+++ Assets/Scripts/vatpham/chiakhoa.cs
@@ -2,22 +2,24 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class chiakhoa : MonoBehaviour
 {
-
+    public string itemName;
     public float pickupDistance = 2f; // Khoảng cách tối đa từ người chơi mà vật phẩm có thể được nhặt lên
     public Transform player;
     [SerializeField] private AudioClip pickup;
     public Image itemImage; // Hình ảnh sẽ hiển thị khi vật phẩm được nhặt lên
-
+    public TextMeshProUGUI itemCountText; // Văn bản hiển thị số lượng vật phẩm còn lại để nhặt lên
+    
 
     // Biến tĩnh để theo dõi số lượng vật phẩm còn lại để nhặt lên
-    public static int itemCount = 0;
+    public static int itemCount = 1;
 
     void Awake()
     {
-        itemImage.enabled = false;
+        itemCount = 1; // Đặt lại itemCount về1 mỗi khi Scene được tải
     }
 
     void Update()
@@ -25,12 +27,13 @@ public class chiakhoa : MonoBehaviour
         // Kiểm tra xem người chơi có trong khoảng cách pickupDistance của vật phẩm và có bất kỳ vật phẩm nào còn lại để nhặt lên không
         if (Vector3.Distance(transform.position, player.position) < pickupDistance && itemCount > 0)
         {
-
-            itemCount++;
+            
+            itemCount--; // Giảm số lượng vật phẩm còn lại để nhặt lên
             itemImage.enabled = true; // Kích hoạt hình ảnh của vật phẩm
-            AudioSource.PlayClipAtPoint(pickup, transform.position);
+            itemCountText.text = itemCount.ToString(); // Cập nhật văn bản để hiển thị số lượng vật phẩm còn lại để nhặt lên
+            AudioSource.PlayClipAtPoint(pickup,transform.position);
             Destroy(gameObject); // Hủy đối tượng trò chơi mà mã nguồn này được gắn vào
 
-        }
+        }  
     }
 }
