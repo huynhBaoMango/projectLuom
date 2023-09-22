f1f245b3d44c431b0b12670002d261295b37fcf4
huynhBaoMango <deathtnt.thebest@gmail.com>
2023-09-22 10:10:31 +0700

    ad

---
+++ Assets/Scripts/laco.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class laco : MonoBehaviour
+{
+
+    Vector3 scale = new Vector3(0.025f, 0.025f, 0.025f);
+    float speed = 2;
+
+    void Awake()
+    {
+        transform.localScale = scale;
+    }
+
+    void Update()
+    {
+        if(transform.localScale.x < 1)
+        {
+            transform.localScale += Vector3.Lerp(scale, transform.localScale, Time.deltaTime * 0.5f);
+            Debug.Log(transform.localScale);
+        }
+    }
+}
